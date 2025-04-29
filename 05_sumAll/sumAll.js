9b547936e86236135311f94b30520ff86fc81ad2
Ternamdoo <jerryaliyese@gmail.com>
2025-04-29 20:00:32 +0100

    Pass first 3 tests in the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let firstNumber;
+    let lastNumber;
 
+    switch (true) {
+        case num1 < num2:
+            firstNumber = num1;
+            lastNumber = num2;
+            break;
+        case num1 > num2:
+            firstNumber = num2;
+            lastNumber = num1;
+            break;
+    }
+    
+    for (let i  = firstNumber; i <= lastNumber; i++) {
+        sum+= i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
