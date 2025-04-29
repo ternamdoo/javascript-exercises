dbcf8f9a0eb7bda9914158b2626a1cb1c7fb9361
Ternamdoo <jerryaliyese@gmail.com>
2025-04-29 10:05:23 +0100

    Satisfy all test cases in the current exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        for (let i = 0; i < array.length; i++) {
+            array = array.filter(item => arg !== item);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
