dea9673bac1137d336e4c962ca36fadb111af189
Ternamdoo <jerryaliyese@gmail.com>
2025-04-29 23:41:50 +0100

    Pass all tests in this exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let toCelsius = (value - 32) * 5 / 9;
+  return Number(toCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let toFahrenheit = (value * 9/5) + 32;
+  return Number(toFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
