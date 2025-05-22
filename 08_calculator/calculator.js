013a5e2ccab71497e37ad915e0df75c86ec0e21f
Ternamdoo <jerryaliyese@gmail.com>
2025-05-22 16:21:44 +0100

    Update factorial function to pass factorial tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,13 @@ const power = function(a, b) {
 	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  let result = num;
+	for (let i = num - 1; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
