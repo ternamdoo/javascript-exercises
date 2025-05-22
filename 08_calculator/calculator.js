7dbd727cc5a0da27bd72db838e8a48ee9a0e5f4c
Ternamdoo <jerryaliyese@gmail.com>
2025-05-22 15:56:25 +0100

    Complete the power function to pass tests 13 & 14

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,8 +14,8 @@ const multiply = function(array) {
   return array.reduce((previousMultiple, currentValue) => previousMultiple * currentValue);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
 const factorial = function() {
